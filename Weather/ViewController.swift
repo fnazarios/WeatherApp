bb29449868958d74d700c39bd6ab1305cb0d361c
Fernando Nazario <fnazarios@gmail.com>
2020-01-31 15:39:54 -0300

    Initial Commit

---
+++ Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Fernando Sousa on 31/01/20.
+//  Copyright © 2020 fnazarios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
